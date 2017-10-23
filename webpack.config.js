e26970116517361ef466a528725758636f6d46b8
Oscar Soriano <oscar.soriano@outlook.com>
2017-10-23 02:19:50 -0600

    Project setup.

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const path = require('path');
+
+module.exports = {
+	entry: ['babel-polyfill', './src/app.js'],
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		publicPath: '/dist/',
+		filename: 'build.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				use: [
+					{
+						loader: 'babel-loader',
+						options: {
+							presets: ['env'],
+						},
+					},
+				],
+			},
+		],
+	},
+	devServer: {
+		contentBase: path.join(__dirname, '/'),
+		historyApiFallback: true,
+		noInfo: true,
+	},
+};
